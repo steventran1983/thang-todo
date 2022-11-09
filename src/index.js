9c33fd6aaee932b37bdf6ea60e72c09a57262f4a
ttran65 <thang.tran65@t-mobile.com>
2022-11-09 00:58:53 -0600

    Thang

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<AppProvider>
-		  <BrowserRouter>
+			<BrowserRouter basename={window.location.pathname || ''}>
 			  <App />
 		  </BrowserRouter>
     </AppProvider>
